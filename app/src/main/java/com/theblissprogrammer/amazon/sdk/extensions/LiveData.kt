d88c74a614da2a778adc7aab388cb80c0d0cfb3c
Ahmed Saad <ahmed.saad@outlook.com>
2019-09-25 13:28:52 -0400

    misc update

---
+++ app/src/main/java/com/theblissprogrammer/amazon/sdk/extensions/LiveData.kt
@@ -0,0 +1,12 @@
+package com.theblissprogrammer.amazon.sdk.extensions
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+
+/**
+ * Created by ahmed.saad on 2019-09-23.
+ * Copyright © 2019. All rights reserved.
+ */
+fun <T, Y> LiveData<T>.switchMap(call: (T) -> Y): LiveData<Y> {
+    return Transformations.map(this, call)
+}
