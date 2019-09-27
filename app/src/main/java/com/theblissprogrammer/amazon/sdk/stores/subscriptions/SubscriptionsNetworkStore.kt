3a0ffcf2e605256d28fb2d7046a262b734942242
Ahmed Saad <ahmed.saad@outlook.com>
2019-09-27 18:48:30 -0400

    Added policy to allow MWS to send messages

--- app/src/main/java/com/theblissprogrammer/amazon/sdk/stores/subscriptions/SubscriptionsNetworkStore.kt
+++ app/src/main/java/com/theblissprogrammer/amazon/sdk/stores/subscriptions/SubscriptionsNetworkStore.kt
@@ -1,20 +1,25 @@
 package com.theblissprogrammer.amazon.sdk.stores.subscriptions
 
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.policy.Policy
+import com.amazonaws.auth.policy.Principal
+import com.amazonaws.auth.policy.Resource
+import com.amazonaws.auth.policy.Statement
+import com.amazonaws.auth.policy.actions.SQSActions
 import com.amazonaws.services.sqs.AmazonSQSClient
 import com.amazonaws.services.sqs.model.CreateQueueRequest
+import com.amazonaws.services.sqs.model.QueueAttributeName
 import com.amazonaws.services.sqs.model.QueueNameExistsException
+import com.amazonaws.services.sqs.model.SetQueueAttributesRequest
 import com.theblissprogrammer.amazon.sdk.common.DeferredResult
-import com.theblissprogrammer.amazon.sdk.common.Result
 import com.theblissprogrammer.amazon.sdk.common.Result.Companion.success
-import com.theblissprogrammer.amazon.sdk.enums.DefaultsKeys
-import com.theblissprogrammer.amazon.sdk.enums.marketplaceFromId
-import com.theblissprogrammer.amazon.sdk.errors.DataError
 import com.theblissprogrammer.amazon.sdk.extensions.coroutineNetwork
 import com.theblissprogrammer.amazon.sdk.preferences.ConstantsType
 import com.theblissprogrammer.amazon.sdk.stores.subscriptions.models.Queue
 import com.theblissprogrammer.amazon.sdk.stores.subscriptions.models.SubscriptionsModels
 import java.lang.Exception
+
+
 
 /**
  * Created by ahmed.saad on 2019-09-26.
@@ -33,7 +38,26 @@ class SubscriptionsNetworkStore(val constants: ConstantsType): SubscriptionsStor
                     .addAttributesEntry("ReceiveMessageWaitTimeSeconds", "20")
 
             val queueUrl = try {
-                sqs.createQueue(createRequest).queueUrl
+                val create = sqs.createQueue(createRequest)
+
+                val getQueueAttributesResult = sqs.getQueueAttributes(create.queueUrl,
+                        listOf(QueueAttributeName.QueueArn.toString()))
+                val queueArn = getQueueAttributesResult.attributes[QueueAttributeName.QueueArn.toString()]
+
+                val allAccessPolicy = Policy("SQSAllAccess", listOf(
+                        Statement(Statement.Effect.Allow)
+                                .withActions(SQSActions.AllSQSActions)
+                                .withPrincipals(Principal.All)
+                                .withId("SQSAllAccessStatement")
+                                .withResources(Resource(queueArn))
+                ))
+
+                sqs.setQueueAttributes(SetQueueAttributesRequest()
+                        .withQueueUrl(create.queueUrl)
+                        .withAttributes(mapOf(QueueAttributeName.Policy.toString() to allAccessPolicy.toJson())))
+
+                create.queueUrl
+
             } catch (e: QueueNameExistsException) {
                 sqs.getQueueUrl(request.name).queueUrl
             } catch (e: Exception) {
